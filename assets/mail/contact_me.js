f4b17e72a8954bcb75b1e9f5a7b1b84486f621dc
Okafor E. Tochukwu <okafor_tochi@yahoo.com>
2022-05-05 17:22:10 +0100

    Update contact_me.js

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "{Insert your own AWS API Gateway Endpoint URL here}",
+                url: "https://nt0etzlh0j.execute-api.eu-west-1.amazonaws.com/lambdafunctionforpredict",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
